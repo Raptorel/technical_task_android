3ebe83bcb0e055316c4ab4afb3c2cf0b830197d6
robert.ruxandrescu <raptorescu@yahoo.com>
2022-04-23 22:04:15 +0300

    Setup basic navigation

--- app/src/main/java/com/sliide/sliideuser/MainActivity.kt
+++ app/src/main/java/com/sliide/sliideuser/MainActivity.kt
@@ -2,16 +2,25 @@ package com.sliide.sliideuser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.sliide.sliideuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val navController: NavController by lazy {
+        findNavController(R.id.navHostFragment)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        navController.setGraph(R.navigation.main_navigation)
+
+        supportActionBar?.hide()
     }
 }
